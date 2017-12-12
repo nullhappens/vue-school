875dd6b9716b136ba8152220794f97f9272e75fb
Diego Pineda <dpineda@nullhappens.com>
2017-12-11 21:51:58 -0500

    Added Not Found page component

---
+++ vueschool-forum/src/components/pages/PageNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-full">
+    <h1>Not Found</h1>
+    <p class="text-large">
+      Oops, we couldn't find what you are looking for.  Try going 
+      <router-link :to="{name: 'PageHome'}">
+        home
+      </router-link>
+      instead?
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 100px;
+  }
+  .text-large {
+    font-size: 50px;
+    font-weight: 100;
+  }
+  div {
+    text-align: center;
+  }
+</style>
--- vueschool-forum/src/router/index.js
+++ vueschool-forum/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import PageHome from '@/components/pages/PageHome';
 import PageThreadShow from '@/components/pages/PageThreadShow';
+import PageNotFound from '@/components/pages/PageNotFound';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       name: 'PageThreadShow',
       component: PageThreadShow,
       props: true,
+    },
+    {
+      path: '*',
+      name: 'PageNotFound',
+      component: PageNotFound,
     },
   ],
   mode: 'history',
